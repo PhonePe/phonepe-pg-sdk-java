65f3415169456394d366838ffc932d63d35e99bc
Shubhransh Jagota <shubhransh.jagota@phonepe.com>
2025-07-10 20:19:25 +0530

    address n cycles containing m classes within package issues

--- src/main/java/com/phonepe/sdk/pg/common/models/request/paymentmodeconstraints/CardPaymentModeConstraint.java
+++ src/main/java/com/phonepe/sdk/pg/common/models/request/paymentmodeconstraints/CardPaymentModeConstraint.java
@@ -18,18 +18,19 @@ package com.phonepe.sdk.pg.common.models.request.paymentmodeconstraints;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.phonepe.sdk.pg.common.models.PgV2InstrumentType;
 import java.util.Set;
 import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
+@JsonTypeName("CARD")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
 public class CardPaymentModeConstraint extends PaymentModeConstraint {
-
     private final Set<CardType> cardTypes;
 
     @Builder
--- src/main/java/com/phonepe/sdk/pg/common/models/request/paymentmodeconstraints/PaymentModeConstraint.java
+++ src/main/java/com/phonepe/sdk/pg/common/models/request/paymentmodeconstraints/PaymentModeConstraint.java
@@ -15,30 +15,26 @@
  */
 package com.phonepe.sdk.pg.common.models.request.paymentmodeconstraints;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.phonepe.sdk.pg.common.models.PgV2InstrumentType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
         include = As.EXISTING_PROPERTY,
         property = "type",
         visible = true)
-@JsonSubTypes({
-    @JsonSubTypes.Type(value = CardPaymentModeConstraint.class, name = "CARD"),
-    @JsonSubTypes.Type(value = NetBankingPaymentModeConstraint.class, name = "NET_BANKING"),
-    @JsonSubTypes.Type(value = UpiIntentPaymentModeConstraint.class, name = "UPI_INTENT"),
-    @JsonSubTypes.Type(value = UpiQrPaymentModeConstraint.class, name = "UPI_QR"),
-    @JsonSubTypes.Type(value = UpiCollectPaymentModeConstraint.class, name = "UPI_COLLECT")
-})
 public abstract class PaymentModeConstraint {
 
     protected PgV2InstrumentType type;
+
+    public PaymentModeConstraint(PgV2InstrumentType type) {
+        this.type = type;
+    }
 }
